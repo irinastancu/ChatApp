b9a8996273b2606f22fcfde7ac365064fa3339ad
irinastancu <irina.stancuviziri@gmail.com>
2021-05-11 21:17:49 +0300

    Created ChatServiceImpl class in Server
    In this class we get and return a StreamObserver response observer,via our method’s response observer while the client is still writing messages to their message stream.
    Because we are using bidirectional streaming, both the client and server can read and write in any order — the streams operate completely independently.

---
+++ ChatApp-Server/src/main/java/service/ChatServiceImpl.java
@@ -0,0 +1,48 @@
+package service;
+
+import com.google.protobuf.Timestamp;
+import io.grpc.stub.StreamObserver;
+import proto.Chat;
+import proto.ChatServiceGrpc;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ChatServiceImpl extends ChatServiceGrpc.ChatServiceImplBase {
+
+    private static Set<StreamObserver<Chat.ChatMessageFromServer>> observers = ConcurrentHashMap.newKeySet();
+
+    @Override
+    public StreamObserver<Chat.ChatMessage> chat(StreamObserver<Chat.ChatMessageFromServer> responseObserver) {
+        observers.add(responseObserver);
+
+        return new StreamObserver<Chat.ChatMessage>() {
+            @Override
+            public void onNext(Chat.ChatMessage value) {
+                System.out.println(value);
+                Chat.ChatMessageFromServer message = Chat.ChatMessageFromServer.newBuilder()
+                        .setMessage(value)
+                        .setTimestamp(Timestamp.newBuilder().setSeconds(System.currentTimeMillis() / 1000))
+                        .build();
+
+                for (StreamObserver<Chat.ChatMessageFromServer> observer : observers) {
+                    observer.onNext(message);
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                //TBA
+            }
+
+            @Override
+            public void onCompleted() {
+                observers.remove(responseObserver);
+            }
+        };
+    }
+}
+
